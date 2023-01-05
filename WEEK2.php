07b82e728227c17e1571a2296cfafb71bab349b5
anelbernal <110074128+anelbernal@users.noreply.github.com>
2023-01-04 20:07:51 -0700

    Add files via upload

---
+++ WEEK2.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        
+    </head>
+    <body style="background-color:  rgb(241, 239, 221);" font-weight="" onload="askName()">
+        <h1>Welcome to My Page</h1>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <header>
+            <hr>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
+            <a href="#" class="navbar-brand">Anel's Website</a>
+        
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navContent">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <nav>
+        <ul class="navbar-nav ml-auto">
+            <li class="navbar-item active"><a href="WEEK2.PHP" class="nav-link">Home|</a></li>
+            <li class="navbar-item"><a href="about.php" class="nav-link">About me|</a></li>
+            <li class="navbar-item"><a href="AmbgResume.pdf" class="nav-link">Resume|</a></li>
+            <li class="navbar-item"><a href="portfolio.php" class="nav-link">Portfolio|</a></li>
+            <li class="navbar-item"><a href="contact.php" class="nav-link">Contact me|</a> </li>  
+        </ul>
+    </nav>
+        </header>
+    <div class="container-fluid py-5">
+        <div class="px-4 py-5 my-5 text-center">
+            <div class="row text-black text-center" >
+                <div class="col">
+                    <h2 class="display-4 font-weight-bold">Portfolio</h2>
+                    <hr class="bg-white mb-5">
+        </div>
+    </div>
+
+            
+                <div class="col">
+                    <div class="card bg-light text-center border-light shadow h-100">
+                        <img src="https://tinyurl.com/mpnr7t72" alt="sunflower" class="card-img-top" style="object-fit: cover; height: 15em;"/>
+                        <div class="card-body">
+                            <h3 class="card-title">EXAMPLE 1</h3>
+                            <hr class="bg-primary">
+                            <p class="card-text">This is an example of Example 1</p>
+                        </div>
+                        <div class="card-footer">
+                            <a class="btn btn-outline-primary btn-lg mt-2 type=submit formaction='about.php'">Read More</a>
+                            
+                <div class="col">
+                    <div class="card bg-light text-center border-light shadow h-100">
+                        <img src="https://tinyurl.com/mpnr7t72" alt="sunflower" class="card-img-top" style="object-fit: cover; height: 15em;"/>
+                        <div class="card-body">
+                            <h3 class="card-title">EXAMPLE 2</h3>
+                            <hr class="bg-primary">
+                            <p class="card-text">This is an example of Example 2</p>
+                        </div>
+                        <div class="card-footer">
+                            <a class="btn btn-outline-primary btn-lg mt-2 type='submit' formaction='https://abernal.slccwebdev.com/wp-class/medical-business-free/' ">Read More</a>
+                            
+                <div class="col">
+                    <div class="card bg-light text-center border-light shadow h-100">
+                        <img src="https://tinyurl.com/mpnr7t72" alt="sunflower" class="card-img-top" style="object-fit: cover; height: 15em;"/>
+                        <div class="card-body">
+                            <h3 class="card-title">EXAMPLE 3</h3>
+                            <hr class="bg-primary">
+                            <p class="card-text">This is an example of Example 3</p>
+                        </div>
+                        <div class="card-footer">
+                            <a class="btn btn-outline-primary btn-lg mt-2">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<!-- //*copied from here*// -->
+   <br>
+</body>
+</html>
